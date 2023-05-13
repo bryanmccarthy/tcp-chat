338a0e0712ad204c5075bfd3b6a5c7b0df7c24e5
Bryan McCarthy <bryanmccarthy2@gmail.com>
2023-05-13 12:12:33 +0100

    setup client

--- client/main.c
+++ client/main.c
@@ -1,9 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
 
-int main() {
+#define PORT 8080
 
-  printf("Client\n");
+int main(int argc, char *argv[]) {
+
+  int sock_fd, valread;
+  struct sockaddr_in server_addr;
+  char buffer[1024] = {0};
+
+  // Create socket file descriptor
+  if((sock_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    perror("socket failed");
+    exit(EXIT_FAILURE);
+  }
+
+  // Set server address
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = INADDR_ANY;
+  server_addr.sin_port = htons(PORT);
+
+  // Connect to server
+  if(connect(sock_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
+    perror("connect failed");
+    exit(EXIT_FAILURE);
+  }
+
+  // Client loop
+  while(1) {
+    sleep(5);
+    printf("connected to server\n");
+  }
+
+  close(sock_fd);
 
   return 0;
 }
